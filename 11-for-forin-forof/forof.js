ab3563a92129df1c77883926b4c8f87d578c3096
Sandra Ferla <artkaviah@gmail.com>
2022-07-07 17:37:12 -0300

    código comentado utilizando for...of

---
+++ 11-for-forin-forof/forof.js
@@ -0,0 +1,10 @@
+// FOR OF => percorre valores iterativos de objetos
+// Exemplo:
+
+const alunos = ['Rafael','Fernanda','Gabriel','Jorge'];
+
+for (aluno of alunos) {
+    console.log(aluno);
+}
+
+// forma mais simplificada para printar elementos específicos do array
