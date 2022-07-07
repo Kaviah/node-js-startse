d7599c2769a35327920bc0dcd07b720fa2ee05b8
Sandra Ferla <artkaviah@gmail.com>
2022-07-06 22:10:15 -0300

    exemplo de aplicação de async e await

---
+++ aula10-async-await/index.js
@@ -0,0 +1,26 @@
+import fetch from "node-fetch";
+
+async function main() {
+    const user = 'kaviah';
+    const user2 = 'rprrafa';
+    const apiUrl = 'https://api.github.com';
+    // fetch(`${apiUrl}/users/${user}`)
+    //     .then((response) => {
+    //         return response.json();
+    //     }).then((user) => {
+    //         console.log(user.name);
+    //     })
+    // const response = await fetch(`${apiUrl}/users/${user}`);
+    // const response2 = await fetch(`${apiUrl}/users/${user2}`);
+
+    const promise = fetch(`${apiUrl}/users/${user}`);
+    const promise2 = fetch(`${apiUrl}/users/${user2}`);
+    const promises = await Promise.all([promise,promise2]);
+    const githubUser = await promises[0].json();
+    const githubUser2 = await promises[1].json();
+    console.log(`Sandra ID: ${githubUser.id}`);
+    console.log(`Rafa ID: ${githubUser2.id}`);
+
+}
+
+main();
